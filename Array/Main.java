c821679c9119b89d688b6283aefb02f908d7d54f
prakashabhijeet1-sudo <prakashabhijeet1@gmail.com>
2026-09-20 21:52:17 +0530

    dsa question practice

---
+++ Array/Main.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter value to remove: ");
+        int val = sc.nextInt();
+
+        int k = removeElement(nums, val);
+
+        System.out.println("Number of elements remaining: " + k);
+
+        System.out.print("Array after removing " + val + ": ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        sc.close();
+    }
+}
