cf12673e6b1f6ebdc88c30a2f7e328f335624d00
prakashabhijeet1-sudo <prakashabhijeet1@gmail.com>
2026-09-21 23:25:13 +0530

    dsa question practice

---
+++ Array/minmaxprofit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class minmaxprofit {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("ENTER THE SIZE OF ARRAY:");
+        int n = sc.nextInt();
+        System.out.println("ENTER THE PRICES ACCORDINGLY TO THE DAYWISE ");
+        
+
+    }
+    
+}
