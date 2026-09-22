49caf7ec7bdca1252930c2e368fb24481c6cbab3
prakashabhijeet1-sudo <prakashabhijeet1@gmail.com>
2026-09-22 17:54:06 +0530

    dsa question modified

--- Array/minmaxprofit.java
+++ Array/minmaxprofit.java
@@ -5,8 +5,31 @@ public class minmaxprofit {
         Scanner sc =new Scanner(System.in);
         System.out.println("ENTER THE SIZE OF ARRAY:");
         int n = sc.nextInt();
+        int[] prices = new int[n];
+
         System.out.println("ENTER THE PRICES ACCORDINGLY TO THE DAYWISE ");
-        
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+         System.out.println("MAXIMUM PROFIT = " + maxProfit);
+
+        sc.close();
 
     }
     
